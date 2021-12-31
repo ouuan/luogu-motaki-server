8ec95d38b213a72fe5a3eebb5f571ede4321147e
Yufan You <ouuansteve@gmail.com>
2021-12-31 18:15:20 +0800

    fix: use body instead of params for POST

--- src/finishJob.ts
+++ src/finishJob.ts
@@ -2,10 +2,10 @@ import { FastifyRequest, FastifyReply } from 'fastify';
 import Jobs from './jobs';
 
 export default async function finishJob(jobs: Jobs, req: FastifyRequest, rep: FastifyReply) {
-  const { ip, params } = req;
+  const { ip, body } = req;
   const {
     x, y, uuid, success,
-  } = params as any;
+  } = body as any;
 
   if (typeof x === 'number' && typeof y === 'number' && typeof uuid === 'string' && typeof success === 'boolean') {
     const status = await jobs.report(ip, x, y, uuid, success);
